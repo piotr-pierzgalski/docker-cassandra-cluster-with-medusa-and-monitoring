8e1cb48fc05a667ba99dd15674bc89004a0484f9
Piotr <piotr.pierzgalski@epicode.net>
2020-06-17 13:20:11 +0200

    Git-related line ending mismatch fix, removed node_exporter for now

--- cassandra-containerized/scripts/startvariousstuff.sh
+++ cassandra-containerized/scripts/startvariousstuff.sh
@@ -3,8 +3,5 @@ echo "######### <Running custom script> #########"
 echo "Starting ssh server"
 sudo service ssh start
 
-echo "Starting node exporter"
-sudo nohup ${GOPATH-$HOME/go}/src/github.com/prometheus/node_exporter/node_exporter &
-
 echo "######### </Running custom script> #########"
 exec /docker-entrypoint.sh "$@"
